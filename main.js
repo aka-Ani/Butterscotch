4c47915c668beca201b95704eb13d7c6fa2404a1
Anirudh Thommandram <anirude.t@gmail.com>
2023-04-27 04:14:58 -0400

    build v1
    Added Access Control headers to BrowserWindow requests and responses so that calls to localhost won't have CORS issues

---
+++ main.js
@@ -0,0 +1,58 @@
+const {
+    app,
+    BrowserWindow,
+	session
+} = require('electron')
+
+function createWindow() {
+    // Create the browser window.
+	const mainWindow = new BrowserWindow({
+        width: 1100,
+        height: 750,
+        title: "Butterscotch",
+		autoHideMenuBar: true
+    })
+
+    mainWindow.on('page-title-updated', (evt) => {
+        evt.preventDefault();
+    });
+
+	// Set Access Control headers in response to bypass CORS
+    session.defaultSession.webRequest.onHeadersReceived((details, callback) => {
+        callback({
+            responseHeaders: {
+				'Access-Control-Allow-Origin': ['*'],
+				'Access-Control-Allow-Headers': ['*'],
+				...details.responseHeaders,
+			}
+        });
+    });
+
+    mainWindow.loadURL('https://hoppscotch.io')
+
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', function () {
+        // On macOS it's common to re-create a window in the app when the
+        // dock icon is clicked and there are no other windows open.
+        if (BrowserWindow.getAllWindows().length === 0)
+            createWindow()
+    })
+})
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin')
+        app.quit()
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
